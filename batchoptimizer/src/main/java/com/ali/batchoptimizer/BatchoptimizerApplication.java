a7864ccb6bed1e69f62c8e650c2a0e490e3c57fa
Ali Rekik <medali.rekik.ing@gmail.com>
2025-06-24 16:52:18 +0200

    intialisation project.

---
+++ batchoptimizer/src/main/java/com/ali/batchoptimizer/BatchoptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.ali.batchoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchoptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchoptimizerApplication.class, args);
+	}
+
+}
---
+++ batchoptimizer/src/test/java/com/ali/batchoptimizer/BatchoptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.batchoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchoptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
